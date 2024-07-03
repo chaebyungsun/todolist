232281880f3153d4849b2cc9d2de2c8a6850bc64
chaebyungsun <rhemdgkrtod3@naver.com>
2024-07-04 01:04:48 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,78 @@
-// 유저가 값을 클릭한다
-// + 버튼을 클릭하면 할일이 true가 된다
-// delete를 클릭하면 할일이 삭제된다
-// check를 클릭하면 줄긋기
-// 진행중 끝남 탭을 누르면 언더바가 이동
-// 끝남 탭은 끝난 아이템만 진행중탭은 진행중인 아이템만
-// 전체탭을 누르면 전체 아이템으로 이동
+let taskInput = document.getElementById("task_input"); // 입력 필드를 가져옴
+let addButton = document.getElementById("add_button"); // 추가 버튼을 가져옴
+let taskList = []; // 할 일 목록을 저장할 빈 배열로 초기화
 
-let taskInput = document.querySelector(".task_input");
-let addButton = document.querySelector(".add_button");
-let taskList = []; // 빈 배열로 초기화
-
-addButton.addEventListener("click", addTask);
-
+addButton.addEventListener("click", addTask); // 추가 버튼 클릭 시 addTask 함수 실행
+taskInput.addEventListener("keydown", function (event) {
+  if (event.key === `Enter`) {
+    addTask();
+  }
+});
+// 할 일을 추가하는 함수
 function addTask() {
-  let taskContent = taskInput.value; // taskInput의 밸류를 가져옴
-  taskList.push(taskContent);
-  console.log(taskList);
+  let taskValue = taskInput.value;
+  if (taskValue === "") return alert("할 일을 입력하세요");
+
+  let task = {
+    id: randomIDGenerate(), // 고유 ID 생성
+    taskContent: taskInput.value, // 입력 필드의 값을 할 일 내용으로 저장
+    isComplete: false, // 초기 상태는 완료되지 않음
+  };
+  taskList.push(task); // 새 할 일을 목록에 추가
+  console.log(taskList); // 현재 할 일 목록을 콘솔에 출력
+  taskInput.value = "";
+  render(); // 할 일 목록을 화면에 렌더링
+}
+
+// 할 일 목록을 화면에 렌더링하는 함수
+function render() {
+  let resultHTML = ""; // 결과 HTML을 저장할 변수 초기화
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].isComplete == true) {
+      resultHTML += `<div class="task task_done">
+                <span>${taskList[i].taskContent}</span>
+                <div class="button-box">
+                  <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa fa-rotate-left"></i></button>
+                  <button onclick="deleteTask('${taskList[i].id}')"><i class="fa fa-trash-can"></i></button>
+                </div>
+              </div>`;
+    } else {
+      resultHTML += `<div class="task">
+        <span>${taskList[i].taskContent}</span>
+        <div class="button-box">
+          <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa fa-check"></i></button>
+          <button onclick="deleteTask('${taskList[i].id}')"><i class="fa fa-trash-can"></i></button>
+        </div>
+      </div>`;
+    }
+  }
+  document.getElementById("task_area").innerHTML = resultHTML; // 결과 HTML을 할 일 영역에 삽입
+}
+
+// 할 일 완료 상태를 토글하는 함수
+function toggleComplete(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isComplete = !taskList[i].isComplete; // 완료 상태를 반전
+      break;
+    }
+  }
+  render(); // 변경된 상태를 화면에 렌더링
+  console.log(taskList); // 현재 할 일 목록을 콘솔에 출력
+}
+
+// 할 일을 삭제하는 함수
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList.splice(i, 1);
+      break;
+    }
+  }
   render();
 }
 
-function render() {
-  let resultHTML = "";
-  for (let i = 0; i < taskList.length; i++) {
-    resultHTML += `  <div class="task">
-              <div>${taskList[i]}</div>
-              <div>
-                <button>Check</button>
-                <button>Delete</button>
-              </div>
-            </div>`;
-  }
-  document.getElementById("task_area").innerHTML = resultHTML;
+// 고유 ID를 생성하는 함수
+function randomIDGenerate() {
+  return "_" + Math.random().toString(36).substring(2, 9); // 랜덤한 문자열 생성
 }
